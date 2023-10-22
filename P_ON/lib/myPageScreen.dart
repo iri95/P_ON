0c7fe73bf9686050331da2f5c025a8db322d14d2
정수완 <krkdhs6240@naver.com>
2023-10-23 01:23:56 +0900

    feat: 최초 home 화면 목업작업
    최초 홈화면에 목업작업 완료함 앱바랑 body 작업하고 바텀네비기이션 안함
    
    
    Former-commit-id: 5974ac90bad59a54897998c6d07eb47ca64ef921

---
+++ P_ON/lib/myPageScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class myPageScreen extends StatefulWidget {
+  const myPageScreen({super.key});
+
+  @override
+  State<myPageScreen> createState() => _myPageScreenState();
+}
+
+class _myPageScreenState extends State<myPageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ P_ON/lib/planScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class planScreen extends StatefulWidget {
+  const planScreen({super.key});
+
+  @override
+  State<planScreen> createState() => _planScreenState();
+}
+
+class _planScreenState extends State<planScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
