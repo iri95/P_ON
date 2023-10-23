631591bca97beaa78d6419760ac8a68ed1eaa647
김태환 <kimta2hwan@gmail.com>
2023-10-23 12:08:26 +0900

    Feat: 룸 넘버 레포지토리 생성
    룸 넘버 db 조작을 위한 레포지토리 생성

---
+++ promise/src/main/java/com/wanyviny/promise/roomnum/repository/RoomNumberRepository.java
@@ -0,0 +1,8 @@
+package com.wanyviny.promise.roomnum.repository;
+
+import com.wanyviny.promise.roomnum.domain.RoomNumber;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoomNumberRepository extends CrudRepository<RoomNumber, String> {
+
+}
