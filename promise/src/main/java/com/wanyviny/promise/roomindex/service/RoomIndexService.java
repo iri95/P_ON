e3b1c1df7d026dffdce95a6db67e97f9bf985025
김태환 <kimta2hwan@gmail.com>
2023-10-23 12:21:47 +0900

    Feat: 룸 인덱스 서비스 구현
    룸 인덱스 추상 서비스 생성

---
+++ promise/src/main/java/com/wanyviny/promise/roomindex/service/RoomIndexService.java
@@ -0,0 +1,8 @@
+package com.wanyviny.promise.roomindex.service;
+
+import com.wanyviny.promise.roomindex.domain.dto.RoomIndexDto;
+
+public interface RoomIndexService {
+
+    RoomIndexDto findRoomIndex();
+}
