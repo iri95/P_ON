25701077b3a26380faa6e12cfbda7991c66dbfbc
김태환 <kimta2hwan@gmail.com>
2023-10-23 17:16:29 +0900

    Feat: 메세지 타입 생성
    메세지 타입 enum 객체 생성
    
    
    Former-commit-id: eec21fb01a4af5c685ee016381fcde5c95b1b51b

---
+++ promise/src/main/java/com/wanyviny/promise/message/domain/MessageType.java
@@ -0,0 +1,20 @@
+package com.wanyviny.promise.message.domain;
+
+public enum MessageType {
+
+    ENTER,
+    LEAVE,
+    NOTICE,
+    TEXT,
+    EMOJI,
+    LINK,
+    IMAGE,
+    VIDEO,
+    FILE,
+    MAP,
+    RECORD,
+    INVITE,
+    REFUSE,
+    VOTE_START,
+    VOTE_END
+}
