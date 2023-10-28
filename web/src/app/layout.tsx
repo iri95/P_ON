007ac13fcbb1084222aecc3a37ca98cf1a047e8d
구희영 <9gmldud@naver.com>
2023-10-28 15:43:42 +0900

    Feat: 네이버 검색엔진 등록 meta 태그 작성
    검색 등록ㅇ을 위한 메타태그 추가 작성
    
    Change-Id: Idccb245346b7981db4628c89404a78b6e860675d

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="wXqbRHLSsIryb4TbVEEEO2uBOcxygcDOzvgwhJbvlxw" />
+        <meta name="naver-site-verification" content="8e3fcf0a7a62a2254e119fe92a0e27afbc44a297" />
       </head>
       <body className={inter.className}>
         <Nav />
