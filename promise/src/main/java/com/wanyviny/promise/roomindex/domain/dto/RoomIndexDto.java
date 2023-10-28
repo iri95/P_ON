78260787a23fa4430b9a2ab5b443c4087b34d484
김태환 <kimta2hwan@gmail.com>
2023-10-28 17:03:46 +0900

    Refactor: 파일 제거
    필요없는 파일 제거
    
    Change-Id: If7aadabbcbb517f8979e1fa38968c7c4cb8feeae
    
    Former-commit-id: 2da5d65ed310c47e66f8ba72ed4827307a0452a7

--- promise/src/main/java/com/wanyviny/promise/roomindex/domain/dto/RoomIndexDto.java
+++
@@ -1,6 +0,0 @@
-package com.wanyviny.promise.roomindex.domain.dto;
-
-import lombok.Builder;
-
-@Builder
-public record RoomIndexDto(Long index) {}
--- promise/src/main/java/com/wanyviny/promise/roomindex/repository/RoomIndexRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.wanyviny.promise.roomindex.repository;
-//
-//import com.wanyviny.promise.roomindex.domain.RoomIndex;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface RoomIndexRepository extends CrudRepository<RoomIndex, String> {
-//
-//}
