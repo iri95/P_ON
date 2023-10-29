b7a7f93d4200b74f417865e797bdd9dd5328a9d8
구희영 <9gmldud@naver.com>
2023-10-29 17:45:12 +0900

    Fix: main.dart 추가
    빌드 테스트
    
    Change-Id: Id088ca79010e67bd2a510c9d6b68a6fb4dd25f3f
    
    Former-commit-id: 3e3b70eac632a33caeab7b4427ad22d290a1e425

--- P_ON/android/app/src/main/kotlin/com/fighting/MainActivity.kt
+++ P_ON/android/app/src/main/kotlin/com/fighting/MainActivity.kt
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:P_ON/android/app/src/main/kotlin/com/example/p_on/MainActivity.kt
 package com.wanyviny.p_on
-=======
-package com.fighting
->>>>>>> mobile/develop:P_ON/android/app/src/main/kotlin/com/fighting/MainActivity.kt
 
 import io.flutter.embedding.android.FlutterActivity
 
