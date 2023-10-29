d0d6fef096b26cfc5032a1dc29c37a953897cca0
정수완 <krkdhs6240@naver.com>
2023-10-30 02:01:20 +0900

    Feat: 약속방 생성화면 구현중
    타이틀 전역에 저장해서 넘기고 친구선택부분 UI까지만 만들어둠
    
    Change-Id: I80096767be2dec19223e894d3bb0f7695da2185f
    
    Former-commit-id: eab1006debeb4372d00906ae2edd57fce41dce89

--- P_ON/lib/main.dart
+++ P_ON/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
 import 'app.dart';
@@ -20,5 +21,5 @@ void main() async {
       fallbackLocale: const Locale('en'),
       path: 'assets/translations',
       useOnlyLangCode: true,
-      child: const App()));
+      child: ProviderScope(child: const App())));
 }
--- P_ON/lib/screen/main/tab/promise_room/f_create_promise.dart
+++ P_ON/lib/screen/main/tab/promise_room/f_create_promise.dart
@@ -1,39 +1,95 @@
-import 'package:fast_app_base/common/constant/app_colors.dart';
-import 'package:fast_app_base/common/widget/w_basic_appbar.dart';
+import 'package:after_layout/after_layout.dart';
+import 'package:fast_app_base/screen/main/tab/promise_room/f_selected_friends.dart';
 import 'package:flutter/material.dart';
-import 'package:nav/nav.dart';
 import 'package:velocity_x/velocity_x.dart';
+import '../../../../common/common.dart';
+import '../../../../common/constant/app_colors.dart';
+import '../../../../common/util/app_keyboard_util.dart';
+import '../../../../common/widget/w_basic_appbar.dart';
+import './dto_promise.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart'; // 추가
 
-class CreatePromise extends StatelessWidget {
+
+class CreatePromise extends ConsumerStatefulWidget {
   const CreatePromise({super.key});
+
+  @override
+  ConsumerState<CreatePromise> createState() => _CreatePromiseState();
+}
+
+class _CreatePromiseState extends ConsumerState<CreatePromise> with AfterLayoutMixin {
+  final node = FocusNode();
+  final TextEditingController textController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    textController.addListener(updateText);
+    node.addListener(updateText);
+  }
+
+  void updateText() {
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: const PreferredSize(
-          preferredSize: Size.fromHeight(kToolbarHeight),
-          child: SafeArea(
-            child: BasicAppBar(text: '약속 생성')
-          ),
+      home: SafeArea(
+          child: Scaffold(
+        // extendBody: extendBody,
+        body: Column(
+          children: [
+            const BasicAppBar(text: '약속 생성', isProgressBar: true, percentage: 33),
+            Container(
+                margin: const EdgeInsets.all(24),
+                alignment: Alignment.topLeft,
+                child: '약속명을 입력해 주세요'.text.black.size(16).semiBold.make()),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 24),
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              height: 50,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: textController.text.isEmpty && !node.hasFocus
+                        ? Colors.grey
+                        : AppColors.mainBlue2,
+                  )),
+              child: TextField(
+                focusNode: node,
+                controller: textController,
+                decoration: const InputDecoration(
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none),
+              ),
+            )
+          ],
         ),
-        body: Placeholder(),
         bottomSheet: Container(
           width: double.infinity,
           height: 48,
-          margin: EdgeInsets.symmetric(vertical: 24, horizontal: 24),
-          decoration: BoxDecoration(
-              color: AppColors.mainBlue,
-              borderRadius: BorderRadius.circular(10)
-
-          ),
-          child: TextButton(
-            onPressed: () {},
-            child: '확인'.text.semiBold.white.make(),
+          margin: EdgeInsets.all(24),
+          child: FilledButton(
+            style: FilledButton.styleFrom(
+                backgroundColor: textController.text.isEmpty
+                    ? Colors.grey
+                    : AppColors.mainBlue),
+            onPressed: () {
+              ref.read(promiseProvider.notifier).setPromiseTitle(textController.text);
+              Nav.push(const SelectedFriends());
+            },
+            child: const Text('다음'),
           ),
         ),
-      ),
+      )),
     );
+  }
+
+  @override
+  FutureOr<void> afterFirstLayout(BuildContext context) {
+    AppKeyboardUtil.show(context, node);
   }
 }
