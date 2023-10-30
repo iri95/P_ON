dec052bb459bb52a8ac4da1af812293a5e4af40f
구희영 <9gmldud@naver.com>
2023-10-30 09:10:33 +0900

    Feat: iframe 테스트
    http 테스트
    
    Change-Id: I813b4ecdef1ac96e127b1d45debbe80cbc514c68

--- web/src/app/mobile/page.tsx
+++ web/src/app/mobile/page.tsx
@@ -14,6 +14,20 @@ export default function page() {
         height="500px"
         style={{ border: 'solid black' }}
       />
+      <iframe
+        title="mobileWeb"
+        src="http://k9e102.p.ssafy.io:9090"
+        width="100%"
+        height="500px"
+        style={{ border: 'solid black' }}
+      />
+      <iframe
+        title="mobileWeb"
+        src="http://k9e102.p.ssafy.io"
+        width="100%"
+        height="500px"
+        style={{ border: 'solid black' }}
+      />
       {/* <iframe src="https://www.google.com" width="100%" height="500px" style={{ border: 0 }} /> */}
     </div>
   );
