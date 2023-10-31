e0213768a51791a95b83b9e21be48d99fdad6a23
구희영 <9gmldud@naver.com>
2023-10-31 17:11:55 +0900

    Fix: 사용하지 않는 임포트문 삭제
    사용 안하는 Head 삭제
    
    Change-Id: I12169cb5f2c94b60760f055d65e17dc3dc0ffba4

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Nav from '@/components/NavBar';
-import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
