55cd8367a8b12a28fc19e4f0007fe0b7ee18a8eb
구희영 <9gmldud@naver.com>
2023-10-31 18:48:09 +0900

    Rename: User/ -> user/
    폴더 이름 소문자로 변경
    
    Change-Id: I32b34aecce50e612d7e91c42ffdc0fc4a9257bb1
    
    Former-commit-id: 560e1efc170af2ee4699a2cfac4103f1f9ebfd50

---
+++ user/src/test/java/com/wanyviny/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanyviny.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
