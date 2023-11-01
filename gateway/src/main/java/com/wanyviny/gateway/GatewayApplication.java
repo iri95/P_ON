25fe4fa1e037643d1cb239aad81283c5a325b063
김태환 <kimta2hwan@gmail.com>
2023-11-01 23:39:02 +0900

    Test: 문서화 테스트
    Change-Id: Iadf653ade8dc642dda3e42fb3e3d850c7dd2ada2

--- gateway/src/main/java/com/wanyviny/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/wanyviny/gateway/GatewayApplication.java
@@ -1,13 +1,29 @@
 package com.wanyviny.gateway;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0", description = "Documentation API Gateway v1.0"))
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+//				.route(r -> r.path("/product-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://product-service"))
+//				.route(r -> r.path("/price-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://price-service"))
+				.build();
+	}
 }
