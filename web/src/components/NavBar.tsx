0e4807bb582a72830ffdbb0277bca8b778f644c0
구희영 <9gmldud@naver.com>
2023-11-01 15:20:58 +0900

    Feat: Navbar 구현
    nav 구현 및 스타일링
    
    Change-Id: Ifa53fd8be924fbb903d1c99999538697297336c0

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,29 +1,70 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
-import styles from './NavBar.module.css';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import styles from './NavBar.module.scss';
 
-export default function nav() {
+export default function Nav() {
+  const pathname = usePathname();
+  const [active, setActive] = useState(pathname);
+
+  useEffect(() => {
+    setActive(pathname);
+  }, [pathname]);
+
   return (
-    <header>
-      <Link href="/">
+    <header className={styles['nav-container']}>
+      <Link href="/" className={styles['nav-icon']}>
         <Image alt="Pinky 핑키" src="/Pinky/Pinky1.png" width={50} height={50} />
         <span>P:ON</span>
       </Link>
-      <nav>
-        {/* <section>
-          <Link href="/#section1">section1</Link>
-          <Link href="/#section2">section2</Link>
-          <Link href="/#section3">section3</Link>
-        </section> */}
-        <Link className={styles.navItem} href="/">
-          앱 소개
-        </Link>
-        <Link className={styles.navItem} href="/how-to-use">
-          이용 방법
-        </Link>
-        <Link className={styles.navItem} href="/faq">
-          FAQ
-        </Link>
+      <nav className={styles['nav-items']}>
+        <ul>
+          <li className={`${styles['nav-item']} ${active === '/' ? styles.active : ''}`}>
+            <Link href="/" scroll={false} className={styles['main-item']}>
+              서비스 소개
+              {active === '/' && <div className={styles.line} />}
+            </Link>
+            <ul className={styles.dropdown}>
+              <li>
+                <Link href="#section1">Section1</Link>
+              </li>
+              <li>
+                <Link href="#section2">Section2</Link>
+              </li>
+              <li>
+                <Link href="#section3">Section3</Link>
+              </li>
+              <li>
+                <Link href="#section4">Section4</Link>
+              </li>
+            </ul>
+          </li>
+          <li
+            className={`${styles['nav-item']} ${active === '/how-to-use' || active === '/mobile' ? styles.active : ''}`}
+          >
+            <Link href="/how-to-use" scroll={false} className={styles['main-item']}>
+              가이드
+              {(active === '/how-to-use' || active === '/mobile') && <div className={styles.line} />}
+            </Link>
+            <ul className={styles.dropdown}>
+              <li>
+                <Link href="/how-to-use">P:ON 사용법</Link>
+              </li>
+              <li>
+                <Link href="/mobile">미리보기</Link>
+              </li>
+            </ul>
+          </li>
+          <li className={`${styles['nav-item']} ${active === '/faq' ? styles.active : ''}`}>
+            <Link href="/faq" scroll={false} className={styles['main-item']}>
+              FAQ
+              {active === '/faq' && <div className={styles.line} />}
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
