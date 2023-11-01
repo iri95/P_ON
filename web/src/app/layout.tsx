266d97a4c4be9fe19336ceb870583f6292f67f9c
구희영 <9gmldud@naver.com>
2023-11-01 09:24:25 +0900

    Fix: layout 누락 push
    변경사항 누락됨
    
    Change-Id: I8e4be98195630df0be19b80009271d180d49760b

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <meta httpEquiv="imagetoolbar" content="no" />
-        <link rel="shortcut icon" href="/icon.png" />
+        <link rel="icon" href="/icon.png" />
       </head>
       <body>
         <Nav />
