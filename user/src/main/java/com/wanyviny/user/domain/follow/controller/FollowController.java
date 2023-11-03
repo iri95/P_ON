e86d4b2241ffd9d71a97344a9cb393d81c3c844e
이상훈 <hoonideal@naver.com>
2023-11-03 15:40:57 +0900

    Fix: User, Follow Controller 수정
    User, Follow Controller에서 SecurityContextHolder -> Access Token으로 id값 받아오도록 수정
    
    
    Former-commit-id: 6be96af083643691cf74624068a3ba789bd5448c

--- user/src/main/java/com/wanyviny/user/domain/follow/controller/FollowController.java
+++ user/src/main/java/com/wanyviny/user/domain/follow/controller/FollowController.java
@@ -4,6 +4,8 @@ import com.wanyviny.user.domain.common.BasicResponse;
 import com.wanyviny.user.domain.follow.dto.FollowDto;
 import com.wanyviny.user.domain.follow.service.FollowService;
 import com.wanyviny.user.domain.user.dto.UserDto;
+import com.wanyviny.user.global.jwt.service.JwtService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,18 +22,19 @@ import java.util.List;
 public class FollowController {
 
     private final FollowService followService;
+    private final JwtService jwtService;
 
     // 팔로잉 조회
     @GetMapping("/following")
 
-    public ResponseEntity<BasicResponse> getFollowing() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    public ResponseEntity<BasicResponse> getFollowing(HttpServletRequest request) {
+        String accessToken = request.getHeader("Authorization").replace("Bearer ", "");
 
-        if (authentication == null || authentication.getName() == null || authentication.getName().equals("anonymousUser")) {
-            throw new RuntimeException("Security Context에 인증 정보가 없습니다.");
-        }
+        Long id = jwtService.extractId(accessToken).orElseThrow(
+                () -> new IllegalArgumentException("Access Token에 해당하는 id가 없습니다.")
+        );
 
-        List<FollowDto> followDtoList =  followService.getFollowing(Long.parseLong(authentication.getName()));
+        List<FollowDto> followDtoList =  followService.getFollowing(id);
 
         BasicResponse basicResponse = BasicResponse.builder()
                 .code(HttpStatus.OK.value())
@@ -46,14 +49,14 @@ public class FollowController {
 
     // 팔로워 조회 -> 서로 팔로우한 경우도 함꼐
     @GetMapping("/follower")
-    public ResponseEntity<BasicResponse> getFollower() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    public ResponseEntity<BasicResponse> getFollower(HttpServletRequest request) {
+        String accessToken = request.getHeader("Authorization").replace("Bearer ", "");
 
-        if (authentication == null || authentication.getName() == null || authentication.getName().equals("anonymousUser")) {
-            throw new RuntimeException("Security Context에 인증 정보가 없습니다.");
-        }
+        Long id = jwtService.extractId(accessToken).orElseThrow(
+                () -> new IllegalArgumentException("Access Token에 해당하는 id가 없습니다.")
+        );
 
-        List<FollowDto> followDtoList =  followService.getFollower(Long.parseLong(authentication.getName()));
+        List<FollowDto> followDtoList =  followService.getFollower(id);
 
         BasicResponse basicResponse = BasicResponse.builder()
                 .code(HttpStatus.OK.value())
@@ -68,14 +71,14 @@ public class FollowController {
 
     // 팔로잉 추가
     @PostMapping("/following/{followingId}")
-    public ResponseEntity<BasicResponse> setFollowing(@PathVariable(name = "followingId") Long followingId) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    public ResponseEntity<BasicResponse> setFollowing(HttpServletRequest request, @PathVariable(name = "followingId") Long followingId) {
+        String accessToken = request.getHeader("Authorization").replace("Bearer ", "");
 
-        if (authentication == null || authentication.getName() == null || authentication.getName().equals("anonymousUser")) {
-            throw new RuntimeException("Security Context에 인증 정보가 없습니다.");
-        }
+        Long id = jwtService.extractId(accessToken).orElseThrow(
+                () -> new IllegalArgumentException("Access Token에 해당하는 id가 없습니다.")
+        );
 
-        followService.setFollowing(Long.parseLong(authentication.getName()), followingId);
+        followService.setFollowing(id, followingId);
 
         BasicResponse basicResponse = BasicResponse.builder()
                 .code(HttpStatus.OK.value())
@@ -88,14 +91,14 @@ public class FollowController {
 
     // 팔로잉 삭제
     @DeleteMapping("/following/{followingId}")
-    public ResponseEntity<BasicResponse> deleteFollowing(@PathVariable(name = "followingId") Long followingId) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    public ResponseEntity<BasicResponse> deleteFollowing(HttpServletRequest request, @PathVariable(name = "followingId") Long followingId) {
+        String accessToken = request.getHeader("Authorization").replace("Bearer ", "");
 
-        if (authentication == null || authentication.getName() == null || authentication.getName().equals("anonymousUser")) {
-            throw new RuntimeException("Security Context에 인증 정보가 없습니다.");
-        }
+        Long id = jwtService.extractId(accessToken).orElseThrow(
+                () -> new IllegalArgumentException("Access Token에 해당하는 id가 없습니다.")
+        );
 
-        followService.deleteFollowing(Long.parseLong(authentication.getName()), followingId);
+        followService.deleteFollowing(id, followingId);
 
         BasicResponse basicResponse = BasicResponse.builder()
                 .code(HttpStatus.OK.value())
@@ -108,14 +111,14 @@ public class FollowController {
 
     // 팔로워 삭제
     @DeleteMapping("/follower/{followerId}")
-    public ResponseEntity<BasicResponse> deleteFollower(@PathVariable(name = "followerId") Long followerId) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    public ResponseEntity<BasicResponse> deleteFollower(HttpServletRequest request, @PathVariable(name = "followerId") Long followerId) {
+        String accessToken = request.getHeader("Authorization").replace("Bearer ", "");
 
-        if (authentication == null || authentication.getName() == null || authentication.getName().equals("anonymousUser")) {
-            throw new RuntimeException("Security Context에 인증 정보가 없습니다.");
-        }
+        Long id = jwtService.extractId(accessToken).orElseThrow(
+                () -> new IllegalArgumentException("Access Token에 해당하는 id가 없습니다.")
+        );
 
-        followService.deleteFollowing(followerId, Long.parseLong(authentication.getName()));
+        followService.deleteFollowing(followerId, id);
 
         BasicResponse basicResponse = BasicResponse.builder()
                 .code(HttpStatus.OK.value())
