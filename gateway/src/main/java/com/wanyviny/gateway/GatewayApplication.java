da35bf6fdebf0ee5e418a11d03f788c93cf697c3
김태환 <kimta2hwan@gmail.com>
2023-11-03 22:46:48 +0900

    Test: 문서화 테스트
    swagger 테스트
    
    Change-Id: If1c182c8decd2240317b69d7e44183011ff46269

--- gateway/src/main/java/com/wanyviny/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/wanyviny/gateway/GatewayApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -22,8 +23,7 @@ public class GatewayApplication {
 	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
 		return builder
 				.routes()
-//				.route(r -> r.path("/product-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://product-service"))
-//				.route(r -> r.path("/price-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://price-service"))
-				.build();
+				.route(r -> r.path("/user-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://user-service"))
+    .build();
 	}
 }
