3f3e229fdf46c5de91fbb4f6031903e411e3ec9c
구희영 <9gmldud@naver.com>
2023-11-06 10:59:20 +0900

    Feat: 메인 기능 위치 잡기
    위치 레이아웃 선정
    
    Change-Id: Ia8d355918c1e6591188dd4406d7328a1bfdd4450

--- web/src/containers/Home/index.tsx
+++ web/src/containers/Home/index.tsx
@@ -1,28 +1,35 @@
-// 'use client';
+'use client';
 
-// import { motion } from 'framer-motion';
+import { useRef } from 'react';
+import { motion, useScroll, useTransform, MotionValue } from 'framer-motion';
+import Image from 'next/Image';
 import styles from './Home.module.scss';
 import Section1 from './Section1';
-import Section2 from './Section2';
 
-export default function index() {
+function useParallax(value: MotionValue<number>, distance: number) {
+  return useTransform(value, [0, 1], [-distance, distance]);
+}
+
+export default function Index() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const y = useParallax(scrollYProgress, 300);
+
   return (
     <div className={`${styles.scroll}`}>
       <div className={styles.section} id="section1">
         <Section1 />
       </div>
-      <div className={styles.section} id="section2">
-        <Section2 />
-      </div>
-      <div className={styles.section}>
-        <div id="section3">
-          <Section2 />
-        </div>
-      </div>
 
-      <div className={styles.section} id="section4">
-        4
-      </div>
+      <section className={`${styles.section} ${styles.section2} ${styles.service}`} id="section2">
+        <motion.div className={styles['text-container']} style={{ y }}>
+          소개
+        </motion.div>
+        <div className={styles['image-container']} ref={ref}>
+          <div className={styles.screen}>{/* <Image /> */}</div>
+        </div>
+      </section>
+
       <div className={`${styles.section} ${styles.lastSection}`} />
     </div>
   );
