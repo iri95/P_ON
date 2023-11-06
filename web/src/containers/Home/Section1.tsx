d6fdd5490789da9dc32ad477240ff1d8e056754b
구희영 <9gmldud@naver.com>
2023-11-06 17:47:59 +0900

    Feat: 메인 마진 설정
    마진 적당히 주기
    
    Change-Id: I5d97af65914b1379f861f65d5bbea8d7124de535

--- web/src/containers/Home/Section1.tsx
+++ web/src/containers/Home/Section1.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import styles from './Home.module.scss';
 import GoogleBtn from './downloadBtn';
 
@@ -5,14 +6,24 @@ export default function Section1() {
   return (
     <div className={styles.section1}>
       <div className={styles.screenshots}>
-        <div className={`${styles.screen}`}>메인1</div>
-        <div className={`${styles.screen}`}>메인2</div>
-        <div className={`${styles.screen}`}>메인3</div>
+        <div className={styles.screen}>
+          <div>홈화면</div>
+        </div>
+        <div className={`${styles.screen}`}>
+          <div>핑키화면</div>
+        </div>
+        <div className={`${styles.screen}`}>
+          <div>추억화면</div>
+        </div>
       </div>
 
       <div className={styles.text}>
-        <p className={styles.title}>P:ON 슬로건</p>
-        <p className={styles.description}>한 줄 임팩트 있는 설명</p>
+        <div className={styles.title}>
+          <Image src="/P_ON.png" alt="P:ON 로고" objectFit="contain" fill quality={100} />
+        </div>
+        <p className={styles.description}>
+          <span>약속의 시작</span>을 완벽하게 연결합니다!
+        </p>
         <GoogleBtn />
       </div>
     </div>
