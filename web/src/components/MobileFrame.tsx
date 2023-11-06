b655aa145fcae930b80f97421e343b0428a4166b
구희영 <9gmldud@naver.com>
2023-11-06 17:44:43 +0900

    Feat: 메인 슬로건 입력 및 로고 넣음
    로고랑 슬로건 입력
    
    Change-Id: Ib9a72f12861349b6f4ad5d282295e076bdd58de2

--- web/src/components/MobileFrame.tsx
+++ web/src/components/MobileFrame.tsx
@@ -1,3 +1,14 @@
-export default function MobileFrame(children) {
-  return <div>{children}</div>;
+import { ReactNode } from 'react';
+import styles from './MobileFrame.module.scss';
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function MobileFrame({ children }: Props) {
+  return (
+    <div className={styles.frame}>
+      <div className={styles.screen}>{children}</div>
+    </div>
+  );
 }
