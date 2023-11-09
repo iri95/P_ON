ae8203603f4198973badf095c60a2b2cbca4b05e
김나연 <kny0628m@gmail.com>
2023-11-09 15:15:25 +0900

    Feat: 챗봇 API 틀
    get, post API 두개 틀만 잡아둠
    
    Change-Id: Ib1669bcd3de8066c068f0be04512a2ecb87ad3e8
    
    Former-commit-id: 339730cf7f6eb3ed56bc965b6ed54bafdf8e9f54

--- chatbot/app/main.py
+++ chatbot/app/main.py
@@ -1,8 +1,25 @@
+from typing import Union
 from fastapi import FastAPI, Header
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+# 일정 생성
+@app.post("/chatbot")
+async def create_calendar(userId: Union[int, None] = Header(default=None, convert_underscores=False, alias="id")):
+    context = {
+        "message": "안녕 쁘롱트",
+        "userId": userId
+    }
+    return context
+
+
+# 일정 조회
 @app.get("/chatbot")
-async def root():
-    return {"message": "Hello World"}
+async def root(userId: Union[int, None] = Header(default=None, convert_underscores=False, alias="id")):
+    context = {
+        "message": "안녕 쁘롱트",
+        "userId": userId
+    }
+    return context
