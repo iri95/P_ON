b8b09bcf3e2c2cbe8c9932016ad0a8ab4f70c0cc
이상훈 <hoonideal@naver.com>
2023-11-10 11:15:25 +0900

    Feat: follow 기능 추가
    follow 기능 추가
    
    
    Former-commit-id: 2b8c34dd0cc64fd6592301d6d474d88b10364d5a

---
+++ user/src/main/java/com/wanyviny/user/domain/alarm/entity/Alarm.java
@@ -0,0 +1,47 @@
+package com.wanyviny.user.domain.alarm.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.wanyviny.user.domain.alarm.ALARM_TYPE;
+import com.wanyviny.user.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ALARMS")
+public class Alarm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ALARM_ID")
+    private Long alarmId;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @Column(name = "ALARM_MESSAGE")
+    private String alarmMessage;
+
+    @Column(name = "ALARM_STATE")
+    @Builder.Default
+    private Boolean alarmState = false;
+
+    @Column(name = "ALARM_DATE")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Builder.Default
+    private Date alarmDate = new Date();
+
+    @Column(name = "ALARM_TYPE")
+    private ALARM_TYPE alarmType;
+
+    @Column(name = "ROOM_ID")
+    private Long roomId;
+
+}
