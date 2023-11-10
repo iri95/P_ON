a39ff1fb8d56c2e3c745bbfcd05042936b29712c
이상훈 <hoonideal@naver.com>
2023-11-10 10:26:10 +0900

    Fix: AlarmRepository update 수정
    AlarmRepository update 수정
    
    
    Former-commit-id: ec5702fb93114a5e107bf85e0b149a3e380d307f

--- alarm/src/main/java/com/wanyviny/alarm/domain/alarm/repository/AlarmRepository.java
+++ alarm/src/main/java/com/wanyviny/alarm/domain/alarm/repository/AlarmRepository.java
@@ -3,6 +3,7 @@ package com.wanyviny.alarm.domain.alarm.repository;
 import com.wanyviny.alarm.domain.alarm.ALARM_TYPE;
 import com.wanyviny.alarm.domain.alarm.entity.Alarm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,11 +19,13 @@ public interface AlarmRepository extends JpaRepository<Alarm, Long> {
     void deleteByAlarmId(Long alarmId);
 
     @Transactional
-    @Query("UPDATE From Alarm a set a.alarmState = true where a.alarmId = :alarmId")
+    @Modifying
+    @Query("UPDATE Alarm a set a.alarmState = true where a.alarmId = :alarmId")
     void updateStateByAlarmId(Long alarmId);
 
     @Transactional
-    @Query("UPDATE From Alarm a set a.alarmState = true where a.user.id = :userId")
+    @Modifying
+    @Query("UPDATE Alarm a set a.alarmState = true where a.user.id = :userId")
     void updateStateAllByUserId(Long userId);
 
     void deleteByUserId(Long userId);
