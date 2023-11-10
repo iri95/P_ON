4c5297b2ecc0c8da654eb52252f15ce88423eeca
HB_Kim <ghkdwp129@gmail.com>
2023-11-10 11:55:12 +0900

    Feat: 검색화면
    검색화면 스크린만 추가
    
    Change-Id: I97fd34b60b501a0f84db7e9f37b6c1730364646e
    
    Former-commit-id: 15e9faa1dc53ae0bd4b8e0940ff9f9329333afbe

---
+++ P_ON/lib/screen/search/s_search.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SearchScreen extends ConsumerStatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  ConsumerState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends ConsumerState<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
