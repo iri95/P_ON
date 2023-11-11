e55e5ae3bcb1cc75024c0dce4ab85b85f286a724
LeeSanghun <hoonideal@naver.com>
2023-11-11 23:15:23 +0900

    Feat: 투표 종료 기능 구현, firebase push, alarm Scheduled 구현
    투표 종료 기능 구현, firebase push, alarm Scheduled 구현
    
    
    Former-commit-id: a3f7d03c479f447e7e8aa8c30f24eaa96280cc2c

--- promise/src/main/java/com/wanyviny/promise/domain/room/repository/RoomRepository.java
+++ promise/src/main/java/com/wanyviny/promise/domain/room/repository/RoomRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    List<Room> findByCompleteIs(boolean complete);
 
     @Modifying
     @Transactional
-    @Query("update Room r set r.complete = true where r.id = :roomId And r.userId = :userId")
-    Integer completeRoom(Long userId, Long roomId);
+    @Query("update Room r set r.complete = true where r.id = :roomId")
+    void completeRoom(Long roomId);
 }
