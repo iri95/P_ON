c8330ae9dfd1c5a58931a56577fe32610fed8ece
이상훈 <hoonideal@naver.com>
2023-11-13 16:25:31 +0900

    Fix: kafka 수정
    모든 일정을 서버 시작 시 kafka로 보내기 구현 수정
    
    
    Former-commit-id: 44d87d48526caee3d84d1a87c54b08d40f1687fa

--- calendar/src/main/java/com/wanyviny/calendar/global/kafka/service/KafkaProducerService.java
+++ calendar/src/main/java/com/wanyviny/calendar/global/kafka/service/KafkaProducerService.java
@@ -1,14 +1,14 @@
 package com.wanyviny.calendar.global.kafka.service;
 
-import com.wanyviny.calendar.domain.calendar.entity.Calendar;
 import com.wanyviny.calendar.domain.calendar.repository.CalendarRepository;
 import com.wanyviny.calendar.global.kafka.dto.KafkaCalendarDto;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+// import jakarta.annotation.PostConstruct;
+// import com.wanyviny.calendar.domain.calendar.entity.Calendar;
+// import java.util.List;
 
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,16 +18,15 @@ public class KafkaProducerService {
     private final CalendarRepository calendarRepository;
 
     public void sendCalendar(KafkaCalendarDto dto) {
-        System.out.println("dto = " + dto.getUserId());
         kafkaTemplate.send(TOPIC, dto);
     }
 
-    @PostConstruct
-    public void initCalendar() {
-        List<Calendar> calendarList = calendarRepository.findAll();
-        calendarList.stream()
-                .map(Calendar::entityToKafka)
-                .toList()
-                .forEach(dto -> kafkaTemplate.send(TOPIC, dto));
-    }
+//    @PostConstruct
+//    public void initCalendar() {
+//        List<Calendar> calendarList = calendarRepository.findAll();
+//        calendarList.stream()
+//                .map(Calendar::entityToKafka)
+//                .toList()
+//                .forEach(dto -> kafkaTemplate.send(TOPIC, dto));
+//    }
 }
