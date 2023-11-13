089448ca83db9fe81c615d5777dfebb5a243bcee
김나연 <kny0628m@gmail.com>
2023-11-13 15:25:51 +0900

    Feat: /chatbot POST API 완성
    ''
    
    Change-Id: I0ceab08c76d4be9c8220e6f629dbefd2886cf0e2
    
    Former-commit-id: 72514a533b010172710ffad5af70b6e2982c72ce

--- chatbot/app/kafka/connect_to_kafka.py
+++ chatbot/app/kafka/connect_to_kafka.py
@@ -1,7 +1,13 @@
+import json
 from kafka3 import KafkaProducer
-producer = KafkaProducer(bootstrap_servers='server2:9092')
 
-# producer = KafkaProducer(bootstrap_servers=["server2:9092","server2:9093","server1:9092"])
-data = "python"
-producer.send(topic='test-topic', value=data.encode('utf-8'))
-producer.flush() # 
+def kafka_producer(message):
+    producer = KafkaProducer(
+        bootstrap_servers="server2:9092",
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')  # JSON 직렬화 설정
+    )
+
+    response = producer.send(topic='test-topic', value=message).get()
+    producer.flush()
+    return response
+
