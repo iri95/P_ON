64a424522e9d2bed9afda657e1b1b678d689b15f
이상훈 <hoonideal@naver.com>
2023-11-13 12:26:35 +0900

    Feat: 모든 일정 kafka로 보내기 구현
    모든 일정을 서버 시작 시 kafka로 보내기 구현
    
    
    Former-commit-id: 2731c84afc25f9d7d95bd9bba382496870105552

--- calendar/src/main/java/com/wanyviny/calendar/global/kafka/service/KafkaProducerService.java
+++ calendar/src/main/java/com/wanyviny/calendar/global/kafka/service/KafkaProducerService.java
@@ -1,18 +1,33 @@
 package com.wanyviny.calendar.global.kafka.service;
 
+import com.wanyviny.calendar.domain.calendar.entity.Calendar;
+import com.wanyviny.calendar.domain.calendar.repository.CalendarRepository;
 import com.wanyviny.calendar.global.kafka.dto.KafkaCalendarDto;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class KafkaProducerService {
     private static final String TOPIC = "from-mysql-json";
     private final KafkaTemplate<String, KafkaCalendarDto> kafkaTemplate;
+    private final CalendarRepository calendarRepository;
 
     public void sendCalendar(KafkaCalendarDto dto) {
         System.out.println("dto = " + dto.getUserId());
         kafkaTemplate.send(TOPIC, dto);
+    }
+
+    @PostConstruct
+    public void initCalendar() {
+        List<Calendar> calendarList = calendarRepository.findAll();
+        calendarList.stream()
+                .map(Calendar::entityToKafka)
+                .toList()
+                .forEach(dto -> kafkaTemplate.send(TOPIC, dto));
     }
 }
