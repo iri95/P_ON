6f351989d6f4774dd39f066eba9918f52e3dff1f
이상훈 <hoonideal@naver.com>
2023-11-13 17:35:13 +0900

    Feat: Kafka firebase push alarm 기능 구현
    Kafka firebase push alarm 기능 구현
    
    
    Former-commit-id: b25112e1dc231610ff5b41af9c7de04014fb3b02

--- calendar/src/main/java/com/wanyviny/calendar/global/kafka/service/KafkaConsumerService.java
+++ calendar/src/main/java/com/wanyviny/calendar/global/kafka/service/KafkaConsumerService.java
@@ -1,11 +1,16 @@
 package com.wanyviny.calendar.global.kafka.service;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import com.wanyviny.calendar.domain.calendar.service.CalendarService;
 import com.wanyviny.calendar.domain.user.repository.UserRepository;
 import com.wanyviny.calendar.global.kafka.dto.KafkaCalendarDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.text.ParseException;
 
@@ -14,9 +19,40 @@ import java.text.ParseException;
 public class KafkaConsumerService {
 
     private final CalendarService calendarService;
+    private final UserRepository userRepository;
+    private final FirebaseMessaging firebaseMessaging;
 
+    @Transactional
     @KafkaListener(topics = "to-mysql-json", groupId = "postCalendar", containerFactory = "kafkaListener")
     public void consume(KafkaCalendarDto dto) throws ParseException {
         calendarService.postSchedule(dto.getUserId(), dto.kafkaToSet());
+
+        String title = "CREATE CALENDAR";
+        String body = dto.getCal().getCalendar_title() + "이 생성되었습니다!!";
+        String token = userRepository.findById(dto.getUserId()).orElseThrow(
+                () -> new IllegalArgumentException("해당 유저가 존재하지 않습니다.")
+        ).getPhoneId();
+
+        firebasePushAlarm(title, body, token);
+
+    }
+
+    @Transactional
+    public void firebasePushAlarm(String title, String Body, String token) {
+        Notification notification = Notification.builder()
+                .setTitle(title)
+                .setBody(Body)
+                .build();
+
+        Message message = Message.builder()
+                .setToken(token)  // 친구의 FCM 토큰 설정
+                .setNotification(notification)
+                .build();
+        try {
+            firebaseMessaging.send(message);
+        } catch (FirebaseMessagingException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("token에 해당하는 유저를 찾을 수 없습니다.");
+        }
     }
 }
--- calendar/src/main/java/com/wanyviny/calendar/global/kafka/service/KafkaProducerService.java
+++ calendar/src/main/java/com/wanyviny/calendar/global/kafka/service/KafkaProducerService.java
@@ -1,10 +1,10 @@
 package com.wanyviny.calendar.global.kafka.service;
 
-import com.wanyviny.calendar.domain.calendar.repository.CalendarRepository;
 import com.wanyviny.calendar.global.kafka.dto.KafkaCalendarDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+// import com.wanyviny.calendar.domain.calendar.repository.CalendarRepository;
 // import jakarta.annotation.PostConstruct;
 // import com.wanyviny.calendar.domain.calendar.entity.Calendar;
 // import java.util.List;
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 public class KafkaProducerService {
     private static final String TOPIC = "from-mysql-json";
     private final KafkaTemplate<String, KafkaCalendarDto> kafkaTemplate;
-    private final CalendarRepository calendarRepository;
+//    private final CalendarRepository calendarRepository;
 
     public void sendCalendar(KafkaCalendarDto dto) {
         kafkaTemplate.send(TOPIC, dto);
