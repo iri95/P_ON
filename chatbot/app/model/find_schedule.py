439e23da00258802af569a3a328cb133e69c6b4c
김나연 <kny0628m@gmail.com>
2023-11-14 09:49:06 +0900

    Feat: 일정 조회 API ing
    json loader
    
    Change-Id: Ib2ee9d8faa7de20f4e1809189cb7fa027205aa46
    
    Former-commit-id: 5085b544869eff917a6dd869374914e453fa49c8

--- chatbot/app/model/find_schedule.py
+++ chatbot/app/model/find_schedule.py
@@ -1,23 +1,18 @@
-import dateparser
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import create_extraction_chain
+from langchain.schema import HumanMessage
+from make_schedule import to_datetime
+from pprint import pprint
 
-def parse_user_input(user_input):
-    # 현재 날짜를 가져옵니다.
-    today = dateparser.parse("today").date()
+from langchain.document_loaders import JSONLoader
 
-    # 입력 문자열을 파싱하여 날짜를 계산합니다.
-    parsed_date = dateparser.parse(user_input, languages=['ko'])
-    if parsed_date:
-        calculated_date = parsed_date.date()
-        return calculated_date
-    else:
-        raise ValueError("날짜를 파싱할 수 없습니다.")
+def find_cal(userId):
+    loader = JSONLoader(
+        file_path=f'../data/cal_{userId}.json',
+        jq_schema='.messages[].content',
+        text_content=False)
 
-# 사용자 입력을 받습니다.
-user_input = input("날짜를 입력하세요: ")
+    data = loader.load()
+    pprint(data)
 
-try:
-    # 사용자 입력을 파싱하여 날짜를 계산합니다.
-    result_date = parse_user_input(user_input)
-    print("계산된 날짜:", result_date)
-except ValueError as e:
-    print(e)
+find_cal(1)
