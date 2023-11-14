1da4902bb2ba21edbb5bfeced4e7f98e2f37fbb3
LeeSanghun <hoonideal@naver.com>
2023-11-14 21:39:54 +0900

    Fix: Kafka consumer config 수정
    Kafka consumer config 수정
    
    
    Former-commit-id: 804e94b848ef53ff9ae33f2fe01c6a1da1183a18

--- calendar/src/main/java/com/wanyviny/calendar/global/config/KafkaConsumerConfig.java
+++ calendar/src/main/java/com/wanyviny/calendar/global/config/KafkaConsumerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -27,7 +28,8 @@ public class KafkaConsumerConfig {
         Map<String, Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "postCalendar");
-
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), new JsonDeserializer<>(KafkaCalendarDto.class));
     }
 
