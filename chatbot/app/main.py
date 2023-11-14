ec9b6dd3822fa1c1f8d187ab3f255be3055f9b63
김나연 <kny0628m@gmail.com>
2023-11-14 10:29:09 +0900

    Fix: 서버 실행 에러나는 코드 주석 처리
    get API 미완성 부분
    
    Change-Id: I3870d852fe54f7dc702f470a068748bf6d01fa9e
    
    Former-commit-id: b824e29ab1f3ab451b9e6b82373f3f2206a37d6c

--- chatbot/app/main.py
+++ chatbot/app/main.py
@@ -1,17 +1,13 @@
 
-# from kafka.connect_to_kafka import kafka_producer
-from typing import Union
-from fastapi import FastAPI, Header
+from kafka.connect_to_kafka import kafka_producer
 from model.make_schedule import UserMessage_to_cal
-from model.find_schedule import convert_date
+# # from model.find_schedule import convert_date
 
-import json
-from datetime import datetime
-
-
+# import json
+# from datetime import datetime
 
 
-# 일정 생성
+# # 일정 생성
 from fastapi import FastAPI, Header, Request
 from typing import Union
 
@@ -28,7 +24,6 @@ async def create_calendar(
 
     res = UserMessage_to_cal(text)
 
-
     topic_message = {
         'userId' : userId,
         'cal' : res
@@ -38,24 +33,34 @@ async def create_calendar(
     return {"res" : topic_message}
 
 
+# # 일정 조회
+# @app.get("/chatbot")
+# async def root(
+#     userId: Union[int, None] = Header(default=None, convert_underscores=False, alias="id"),
+#     text: str = Body(...)
+#     ):
+
+#     target_date = convert_date(text)
+
+#     # json 파일 열기
+#     with open('data/cal.json', 'r') as f:
+#         data = json.load(f)
+
+#     # userID가 1이고 calendar_start_date가 target_date와 일치하는 객체 찾기
+#     result = []
+#     for item in data:
+#         if item['userID'] == userId and item['calendar_start_date'] == target_date:
+#             result.append(item)
+
+
+#     return {"result": result}
+
+
 # 일정 조회
-@app.get("/chatbot")
-async def root(
-    userId: Union[int, None] = Header(default=None, convert_underscores=False, alias="id"),
-    text: str = Body(...)
-    ):
-
-    target_date = convert_date(text)
-
-    # json 파일 열기
-    with open('data/cal.json', 'r') as f:
-        data = json.load(f)
-
-    # userID가 1이고 calendar_start_date가 target_date와 일치하는 객체 찾기
-    result = []
-    for item in data:
-        if item['userID'] == userId and item['calendar_start_date'] == target_date:
-            result.append(item)
-
-
-    return {"result": result}
+@app.get("/")
+async def root(userId: Union[int, None] = Header(default=None, convert_underscores=False, alias="id")):
+    context = {
+        'userId' : userId,
+        'message' : 'hi'
+    }
+    return context
--- chatbot/app/model/find_schedule.py
+++ chatbot/app/model/find_schedule.py
@@ -1,18 +1,18 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import create_extraction_chain
 from langchain.schema import HumanMessage
-from make_schedule import to_datetime
+from model.make_schedule import to_datetime
 from pprint import pprint
 
 from langchain.document_loaders import JSONLoader
 
-def find_cal(userId):
-    loader = JSONLoader(
-        file_path=f'../data/cal_{userId}.json',
-        jq_schema='.messages[].content',
-        text_content=False)
+# def find_cal(userId):
+#     loader = JSONLoader(
+#         file_path=f'../data/cal_{userId}.json',
+#         jq_schema='.messages[].content',
+#         text_content=False)
 
-    data = loader.load()
-    pprint(data)
+#     data = loader.load()
+#     pprint(data)
 
-find_cal(1)
+# find_cal(1)
