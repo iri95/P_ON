80ede1e23cefcfc806cb1f5ac49c6d0520540890
NayeonKim <kny0628m@gmail.com>
2023-11-14 22:12:27 +0900

    Fix: 카프카, fastapi
    Former-commit-id: 746662dd289a4c5cd29e05016ee218a9ee4324d6

--- chatbot/app/kafka/connect_to_kafka.py
+++ chatbot/app/kafka/connect_to_kafka.py
@@ -6,7 +6,7 @@ from kafka3 import KafkaProducer, KafkaConsumer
 def kafka_producer(message):
     producer = KafkaProducer(
         bootstrap_servers="server2:9092",
-        value_serializer=lambda v: json.dumps(v).encode('utf-8')  # JSON 직렬화 설정
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')  
     )
 
     response = producer.send(topic='from-chatbot-json', value=message).get()
@@ -28,7 +28,7 @@ def kafka_consumer():
         userId = data['userId']
         cal = data['cal']
     
-        file_path = f'../data/cal_{userId}.csv'
+        file_path = f'/S09P31E102/chatbot/app/data/cal_{userId}.csv'
         user_df = pd.DataFrame({'userId': [userId]})
         cal_df = pd.DataFrame([cal])
         df = pd.concat([user_df, cal_df], axis=1)
@@ -41,6 +41,6 @@ def kafka_consumer():
         combined_df = pd.concat([existing_df, df], ignore_index=True)
         combined_df.to_csv(file_path, index=False)
         
-        return f'Save to {file_path}'
+    return 'Kafka Consume Processing complete'
 
-
+# kafka_consumer()
