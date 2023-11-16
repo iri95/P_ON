6f0f4d096f251873fd37c176252f91aee42e394b
구희영 <9gmldud@naver.com>
2023-11-16 10:51:20 +0900

    Feat: 주석 제거
    Change-Id: I900632e9fa43d2351e92b1ea0dd3da9b62f7d1ca
    
    Former-commit-id: 918ff3238ea9982928a75c331a76b02c270dc5ab

--- P_ON/lib/screen/main/user/fn_kakao.dart
+++ P_ON/lib/screen/main/user/fn_kakao.dart
@@ -16,37 +16,6 @@ import 'package:p_on/common/util/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-// 디바이스 아이디 받아오기
-Future<String> getMobileId() async {
-  // DeviceInfoPlugin 클래스 생성
-  final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
-  // id 저장할 변수
-  String id = '';
-  try {
-    // 플랫폼 확인
-    if (Platform.isAndroid) {
-      // 안드로이드의 경우 androidInfo를 이용
-      // 이때 await으로 데이터 받을때까지 대기를 해야함
-      final AndroidDeviceInfo androidData = await deviceInfoPlugin.androidInfo;
-      // 전달 받은 변수의 멤버 변수인 androidId가 고유 id이다.
-      id = androidData.androidId;
-    } else if (Platform.isIOS) {
-      // iOS의 경우 iosInfo를 이용
-      // 이때 await으로 데이터 받을때까지 대기를 해야함
-      final IosDeviceInfo iosData = await deviceInfoPlugin.iosInfo;
-      // 전달 받은 변수의 멤버 변수인 identifierForVendor가 고유 id이다.
-      id = iosData.identifierForVendor;
-    }
-  } on PlatformException {
-    // 어떠한 원인으로 실패할 경우
-    id = '';
-  }
-  // id 한번 출력해보고
-  print('id: $id');
-  // 값 리턴
-  return id;
-}
-
 // FCM 토큰 받아오기
 Future getFCMToken() async {
   String? token;
